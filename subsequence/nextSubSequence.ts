ad88527162479f087c1adbe0c3077c9f7ec6c7f5
Anonts <anon.ts@outlook.com>
2021-01-07 01:24:30 +0700

    init file

---
+++ subsequence/nextSubSequence.ts
@@ -0,0 +1,31 @@
+
+class nextSequenceNumber {
+   public sequence: number[];
+   constructor(
+      numbers: number[]
+   ) {
+      this.sequence = numbers
+   }
+
+   devider(sequence: number[], n: number, start: number) {
+      let result = 0;
+      for (let index = start; index < sequence.length; index++) {
+         result = sequence[index] / n
+      }
+      return result;
+   }
+
+   start() {
+      if (this.sequence.length) {
+         const result: number = this.devider(
+            this.sequence,
+            2,
+            0);
+         this.sequence.push(result);
+         console.log(this.sequence, 'Next sequence is', result);
+      }
+   }
+}
+
+const next = new nextSequenceNumber([72, 36, 18, 9, 4.5]);
+next.start()
